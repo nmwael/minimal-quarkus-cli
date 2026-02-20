a180ee3b355ff03d0e80bbe98093936c767c5702
Nino Martinez Wael <nmwa@tdc.nuuway.dk>
2026-02-20 10:28:03 +0000

    refactoring
    Signed-off-by: Nino Martinez Wael <nmwa@tdc.nuuway.dk>

---
+++ springboot-shell/src/main/java/dk/nmwael/springboot/cli/SpringShellApplication.java
@@ -0,0 +1,23 @@
+package dk.nmwael.springboot.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableCommand(SpringShellApplication.class)
+public class SpringShellApplication {
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = new AnnotationConfigApplicationContext(SpringShellApplication.class);
+		ShellRunner runner = context.getBean(ShellRunner.class);
+		runner.run(args);
+	}
+
+	@Command(name = "hello", description = "Say hello to a given name", group = "Greetings",
+			help = "A command that greets the user with 'Hello ${name}!'. Usage: hello [-n | --name]=<name>")
+	public void sayHello(@Option(shortName = 'n', longName = "name", description = "the name of the person to greet",
+			defaultValue = "World") String name) {
+		System.out.println("Hello " + name + "!");
+	}
+
+}
---
+++ springboot-shell/src/test/java/dk/nmwael/springboot/SpringbootCliApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.nmwael.springboot.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
